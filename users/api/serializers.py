484e1c70bc14b335213df0cbed70a59a65883313
Dorian Gonzalez <doriandarren1@gmail.com>
2026-04-07 21:20:58 +0200

    add models

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -5,3 +5,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'first_name', 'last_name', 'password', 'is_active', 'is_staff']
+        
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
